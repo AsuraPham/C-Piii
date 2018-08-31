3da507aa1f7b45277cf3719755e3dda1095bcdb1
bangpv <phamvanbang1202@gmail.com>
2018-08-31 18:05:01 +0700

    ado with mysql

---
+++ 1. Database manipulation/Ado.net/Ado.net_stored procedues/Ado_mysql/Models/StudentAdo.cs
@@ -0,0 +1,110 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Ado_mysql.Models
+{
+    public class StudentAdo
+    {
+        string conString = @"server=localhost;userid=root;password=1202;database=sinhvien";
+        // view all student
+        public IEnumerable<StudentModel> GetAll()
+        {
+            List<StudentModel> listSt = new List<StudentModel>();
+            using(MySqlConnection mysql=new MySqlConnection(conString))
+            {
+                MySqlCommand com = new MySqlCommand("spGetAllStudent", mysql);
+                com.CommandType = CommandType.StoredProcedure;
+                mysql.Open();
+                MySqlDataReader mySqlDataReader = com.ExecuteReader();
+                while (mySqlDataReader.Read())
+                {
+                    StudentModel st = new StudentModel();
+                    st.Name = mySqlDataReader["Name"].ToString();
+                    st.Date =Convert.ToDateTime(mySqlDataReader["Date"]);
+                    st.Address = mySqlDataReader["Address"].ToString();
+                    st.Phone = mySqlDataReader["Phone"].ToString();
+                    st.Email = mySqlDataReader["Email"].ToString();
+                    listSt.Add(st);
+                }
+                mysql.Close();
+            }
+            return listSt;
+        }
+        //view student by id
+        public StudentModel GetById(int? id)
+        {
+            StudentModel st = new StudentModel();
+            using(MySqlConnection mysql=new MySqlConnection(conString))
+            {
+                string query = "select * from student where id=" + id;
+                MySqlCommand com = new MySqlCommand(query, mysql);
+                mysql.Open();
+                MySqlDataReader mySqlDataReader = com.ExecuteReader();
+                while (mySqlDataReader.Read())
+                {
+                    st.Id = Convert.ToInt32(mySqlDataReader["Id"]);
+                    st.Name = mySqlDataReader["Name"].ToString();
+                    st.Date = Convert.ToDateTime(mySqlDataReader["Date"]);
+                    st.Address = mySqlDataReader["Address"].ToString();
+                    st.Phone = mySqlDataReader["Phone"].ToString();
+                    st.Email = mySqlDataReader["Email"].ToString();
+                }
+                mysql.Close();
+                return st;
+            }
+        }
+        // create student
+        public void AddStudent(StudentModel st)
+        {
+            using (MySqlConnection mySql = new MySqlConnection(conString))
+            {
+                MySqlCommand com = new MySqlCommand("spAddStudent", mySql);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("name1", st.Name);
+                com.Parameters.AddWithValue("date1", st.Date);
+                com.Parameters.AddWithValue("address", st.Address);
+                com.Parameters.AddWithValue("phone", st.Phone);
+                com.Parameters.AddWithValue("email", st.Email);
+                mySql.Open();
+                com.ExecuteNonQuery();
+                mySql.Close();
+            }
+        }
+        //Update student
+        public void UpdateStudent(StudentModel st)
+        {
+            using (MySqlConnection mySql = new MySqlConnection(conString))
+            {
+                MySqlCommand com = new MySqlCommand("spUpdateStudent", mySql);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("Id", st.Id);
+                com.Parameters.AddWithValue("name1", st.Name);
+                com.Parameters.AddWithValue("date1", st.Date);
+                com.Parameters.AddWithValue("address", st.Address);
+                com.Parameters.AddWithValue("phone", st.Phone);
+                com.Parameters.AddWithValue("email", st.Email);
+                mySql.Open();
+                com.ExecuteNonQuery();
+                mySql.Close();
+            }
+        }
+
+        // delete student
+        public void DeleteStudent(int? id)
+        {
+            using(MySqlConnection mySql=new MySqlConnection(conString))
+            {
+                string query = "delete from student where Id=" + id;
+                MySqlCommand com = new MySqlCommand(query, mySql);
+                mySql.Open();
+                com.ExecuteNonQuery();
+                mySql.Close();
+            }
+        }
+    }
+}
---
+++ 4. C# Advance/Thread/Thread/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace demoThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=10;
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine("1"); 
+                    break;
+                case 2:
+                    Console.WriteLine("2");
+                    break;
+                default:
+                    break;
+                    
+            }
+            //Thread t1 = new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread t1 started");
+            //});
+            //t1.IsBackground = true;  // 
+            //t1.Start();
+            //Console.WriteLine("Main thread ending");
+            Console.ReadKey();
+        }
+        
+    }
+}
